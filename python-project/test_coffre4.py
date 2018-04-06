6b6df2b3175aa0d437fe92069dbcfeaad924ecd6
Patrick Lafrance <patrick.lafrance@gmail.com>
2018-04-06 00:03:54 -0400

    Correction étape 4

--- python-project/test_coffre4.py
+++ python-project/test_coffre4.py
@@ -1,40 +1,44 @@
 import builtins
+import time
+import os
 
 anc_input = builtins.input
 échec = True
 
-print("TECHIO> terminal -i 'ls /project/target'")
+
+def send_msg(channel, msg):
+    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
 
 
-# def send_msg(channel, msg):
-#    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
-#
-#
-# def success():
-#    print("TECHIO> success true")
-#
-#
-# def fail():
-#    print("TECHIO> success false")
-#
-#
-# def nouv_input(*params):
-#    global échec
-#    échec = False
-#    if len(params) > 0:
-#        send_msg("Très bien!"
-#                 "Remarquez que le texte mis entre les parenthèses sert de question posée à l'utilisateur.")
-#
-#
-#builtins.input = nouv_input
-# try:
-#    import coffre4
-# except Exception as e:
-#    échec = True
-#    print(e)
-#
-# if échec:
-#    fail()
-#    send_msg("Il manque quelque chose!",
-#             "Placez «entrée = input()» après le message de bienvenue.")
-#
+def success():
+    print("TECHIO> success true")
+
+
+def fail():
+    print("TECHIO> success false")
+
+
+def nouv_input(*params):
+    global échec
+    échec = False
+    if len(params) > 0:
+        send_msg("Très bien!"
+                 "Remarquez que le texte mis entre les parenthèses sert de question posée à l'utilisateur.")
+
+    print("TECHIO> terminal -i 'bash -c \'read in && echo $in > /tmp/entree \''")
+
+    while not os.path.isfile('/tmp/entree'):
+        time.sleep(1)
+
+
+builtins.input = nouv_input
+try:
+    import coffre4
+except Exception as e:
+    échec = True
+    print(e)
+
+if échec:
+    fail()
+    send_msg("Il manque quelque chose!",
+             "Placez «entrée = input()» après le message de bienvenue.")
