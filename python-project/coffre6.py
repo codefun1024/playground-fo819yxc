c814c13ec7ff13f4a12ee8263fa1ee67c6fdf7d1
Patrick Lafrance <patrick.lafrance@gmail.com>
2018-04-06 10:38:43 -0400

    modif étape 6

--- python-project/coffre6.py
+++ python-project/coffre6.py
@@ -6,7 +6,7 @@ combinaison = randint(100, 999)
 # Affichage de la combinaison
 print("Bienvenue au CTF (Coffre-Trop-Fort). Confiez-moi tous vos biens!")
 
-entrée = input()
+entrée = input("Entrez la combinaison du coffre : ")
 
 if placez_la_condition_ici:
     print("C'est la bonne combinaison!")
