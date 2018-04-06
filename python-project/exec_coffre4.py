65c0950165894b3a4445b3b095008e999c67e09f
Patrick Lafrance <patrick.lafrance@gmail.com>
2018-04-06 00:27:36 -0400

    Correction étape 4

--- python-project/exec_coffre4.py
+++ python-project/exec_coffre4.py
@@ -1,1 +1,1 @@
-print("TECHIO> terminal -i 'bash -c \'python3 -m /project/target/coffre4.py |tee /tmp/sortie  \''")
+print("TECHIO> terminal -i 'bash -c \'python3 /project/target/coffre4.py |tee /tmp/sortie  \''")
