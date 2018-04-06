f7aa2dd5e555b37377c33140e442a0cf5be26b60
Patrick Lafrance <patrick.lafrance@gmail.com>
2018-04-05 22:38:20 -0400

    correction du test étape 3

--- python-project/test_coffre3.py
+++ python-project/test_coffre3.py
@@ -1,7 +1,7 @@
 import builtins
 
 anc_print = builtins.print
-échec = False
+échec = True
 
 
 def send_msg(channel, msg):
@@ -19,25 +19,27 @@ def fail():
 def nouv_print(*params):
     global échec
     if len(params) == 1 and params[0] == "Bienvenue au CTF (Coffre-Trop-Fort). Confiez-moi tous vos biens!":
-        succes()
+        success()
         send_msg(
             "Bravo!", "Vous maîtrisez maintenant l'affichage de texte")
+        échec = False
     else:
         fail()
         échec = True
-        send_msg(
-            "Ce n'est pas exactement ça", "Avez-vous recopié la phrase exactement? Sélectionnez le texte de la phrase grâce à la souris puis utiliser le bouton de droite pour copier le texte et enfin le coller à l'endroit désiré.")
-    anc_print(params)
+
+        anc_print(params)
 
 
 builtins.print = nouv_print
 try:
     import coffre3
 
-    if not échec:
-        success()
-        send_msg("Parfait!", "La combinaison restera désormais secrète.")
-except:
+    if échec:
+        fail()
+        send_msg("Pas tout à fait", "Quelque chose ne va pas. Utilisez ```print(\"message à afficher\")``` à la ligne 6 en vous assurant de bien copier le message entre les guillemets anglais.")
+
+except Exception as e:
     fail()
     échec = True
-    send_msg("Quelque chose cloche". "Avez-vous mis les guillemets anglais \"...\" à chaques bouts de la phrase à afficher?")
+    send_msg("Quelque chose cloche",
+             "Avez-vous mis les guillemets anglais \"...\" à chaques bouts de la phrase à afficher?")
